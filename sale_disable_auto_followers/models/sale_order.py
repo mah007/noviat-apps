b0a235d5c62416e5009527b92ff2514db60a62c7
Mahmoud Abdel Latif <mah008@me.com>
2017-12-12 12:52:48 +0200

    Update sale_order.py

--- sale_disable_auto_followers/models/sale_order.py
+++ sale_disable_auto_followers/models/sale_order.py
@@ -2,7 +2,7 @@
 # Copyright 2009-2016 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, api
+from odoo import models, api
 
 import logging
 _logger = logging.getLogger(__name__)
