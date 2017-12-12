2e43464841b0c514f2786ebb4ef418487d1ff2bb
Mahmoud Abdel Latif <mah008@me.com>
2017-12-12 12:54:57 +0200

    Update mail_compose_message.py

--- sale_disable_auto_followers/wizard/mail_compose_message.py
+++ sale_disable_auto_followers/wizard/mail_compose_message.py
@@ -2,7 +2,7 @@
 # Copyright 2009-2016 Noviat
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import api, models
+from odoo import api, models
 
 import logging
 _logger = logging.getLogger(__name__)
