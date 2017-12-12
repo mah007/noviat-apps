89a8419baa5ac64f8cace5de61aa271c35e7c51b
Mahmoud Abdel Latif <mah008@me.com>
2017-12-12 12:54:21 +0200

    Update and rename __openerp__.py to __manifest__.py

--- sale_disable_auto_followers/__manifest__.py
+++ sale_disable_auto_followers/__manifest__.py
@@ -3,10 +3,10 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Disable Automatic Followers on Sale Orders and Invoices',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Noviat',
-    'website': 'http://www.noviat.com',
+    'website': 'http://www.mah007.com',
     'category': 'Sale',
     'depends': ['portal_sale'],
     'installable': True,
